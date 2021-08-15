1f5e9d622bc8b6672ae477168194512bdf8cc868
Eren BALCI <erenbalci3939@gmail.com>
2021-08-15 16:41:23 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,39 @@
+import { FastifyPluginCallback } from "fastify";
+import fp from "fastify-plugin";
+import { Server, ServerOptions } from "socket.io";
+
+export type SocketIoOptions = Partial<ServerOptions>;
+
+const socketPlugin: FastifyPluginCallback<SocketIoOptions> = (
+  fastify,
+  options,
+  next
+) => {
+  if (fastify.io)
+    return next(new Error("fastify-socket.io-plugin has been defined before"));
+
+  fastify
+    .decorate("io", require("socket.io")(fastify.server, options))
+    .decorateRequest("io", fastify.io)
+    .addHook("onClose", (_, done) => {
+      _.io.close();
+      done();
+    });
+  next();
+};
+
+declare module "fastify" {
+  interface FastifyInstance {
+    io: Server;
+  }
+  interface FastifyRequest {
+    io: Server;
+  }
+}
+
+const fastifySocketIO = fp(socketPlugin, {
+  fastify: "3.x",
+  name: "fastify-socket.io-plugin",
+});
+
+export default fastifySocketIO;
